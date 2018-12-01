e52be3ff6764697762dd98a52bd134beee873454
Bordiuzha_Viktor <victor30608@yandex.ru>
2018-12-01 11:56:28 +0300

    up

---
+++ src/sample/Viewer/Main.java
@@ -0,0 +1,29 @@
+package sample.Viewer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("map.fxml"));
+        primaryStage.setTitle("Delivery");
+        primaryStage.getIcons().add(new Image("sample/assets/delivryicon.png"));
+
+        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        // cl.initialize();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
